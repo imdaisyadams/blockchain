fc9b20950f58e34a28f8fda6a93e6ef338ce0392
daisymuri <daisy21murillo@gmail.com>
2024-03-25 22:50:20 -0700

    building out methods

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,6 +3,7 @@ extern crate serde;
 extern crate serde_json;
 extern crate sha2;
 
+use serde::Serialize;
 use serde_derive::Serialize;
 use sha2::{Digest, Sha256};
 use std::fmt::Write;
@@ -37,4 +38,137 @@ pub struct Chain {
     difficulty: u32,
     miner_addr: String,
     reward: f32,
+}
+
+impl Chain {
+    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
+        let mut chain = Chain {
+            chain: Vec::new(),
+            curr_trans: Vec::new(),
+            difficulty,
+            miner_addr,
+            reward: 100.0,
+        };
+
+        chain.generate_new_block();
+        chain
+    }
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transaction {
+            sender,
+            receiver,
+            amount,
+        });
+
+        true
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = match self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap()
+        };
+
+        Chain::hash(&block.header)
+    }
+
+    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+        true
+    }
+
+    pub fn update_reward(&mut self, reward: f32) -> bool {
+        self.reward = reward;
+        true
+    }
+
+    pub fn generate_new_block(&mut self) -> bool {
+        let header = BlockHeader {
+            timestamp: time::now().to_timespec().sec,
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            difficulty: self.difficulty,
+        };
+
+        let reward_trans = Transaction {
+            sender: String::from("root"),
+            receiver: self.miner_addr.clone(),
+            amount: self.reward,
+        };
+
+        let mut block = Block {
+            header,
+            count: 0,
+            transaction: vec![],
+        };
+
+        block.transaction.push(reward_trans);
+        block.transaction.append(&mut self.curr_trans);
+        block.count = block.transaction.len() as u32;
+        block.header.merkle = Chain::get_merkle(block.transaction.clone());
+        Chain::proof_of_work(&mut block.header);
+
+        println!("{:?}", &block);
+        self.chain.push(block);
+        true
+    }
+
+    fn get_merkle(curr_trans: Vec<Transaction>) -> String {
+        let mut merkle = Vec::new();
+
+        for t in &curr_trans {
+            let hash = Chain::hash(t);
+            merkle.push(hash);
+        }
+
+        if merkle.len() % 2 == 1 {
+            let last = merkle.last().clone().unwrap();
+            merkle.push(last);
+        }
+
+        while merkle.len() > 1 {
+            let mut h1 = merkle.remove(0);
+            let mut h2 = merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh = Chain::hash(&h1);
+            merkle.push(nh);
+        }
+
+        merkle.pop().unwrap()
+    }
+
+    pub fn proof_of_work(header: &mut BlockHeader) {
+        loop {
+            let hash = Chain::hash(header);
+            let slice = &hash[..header.difficulty as usize];
+            match slice.parse::<u32>() {
+                Ok(val) => {
+                    if val != 0 {
+                        header.nonce += 1;
+                    } else {
+                        println!("Block hash: {}", hash);
+                        break;
+                    }
+                },
+                Err(_) => {
+                    header.nonce += 1;
+                    continue;
+
+                }
+            }
+        }
+    }
+
+    pub fn hash<T: serde::Serialize>(item: &T) -> String {
+        let input = serde_json::to_string(&item).unwrap();
+        let mut hasher = Sha256::default();
+
+        hasher.input(input.as_bytes());
+        let result = hasher.result();
+        let vec_result = result.to_vec();
+
+        Chain::hex_to_string(vec_result.as_slice())
+    }
+
 }
